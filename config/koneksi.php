bbfb477339155078bfab2e2e0f86711f99ddffe0
Restu Putra <60053761+reystwo@users.noreply.github.com>
2026-01-12 00:55:51 +0700

    Upload koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,1 +1,10 @@
+<?php
+session_start();
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
+try {
+    $conn = new mysqli("localhost", "root", "", "stok_gudang");
+    $conn->set_charset("utf8");
+} catch (Exception $e) {
+    die("Koneksi database gagal: " . $e->getMessage());
+}
